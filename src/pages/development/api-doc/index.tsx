18b2e2b1b99beb96ef6effb938e85097e0d996c8
Shuai <szheng3@uci.edu>
2020-07-01 12:46:00 -0500

    intercept

--- src/pages/development/api-doc/index.tsx
+++ src/pages/development/api-doc/index.tsx
@@ -1,14 +1,17 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React, { useState, useEffect, useRef } from 'react';
-import { Card, Spin } from 'antd';
+import React from 'react';
+import { Card } from 'antd';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import { get } from '@/utils/StorageUtil';
 
 export default () => {
   return (
     <PageHeaderWrapper>
       <Card bordered={false}>
-        <SwaggerUI url="https://splice.passgpa.com/v2/api-docs" />
+        <SwaggerUI url="https://splice.passgpa.com/v2/api-docs" requestInterceptor={req => {
+          req.headers.Authorization = `Bearer ${get('sso')}`;
+         return  req}}/>
       </Card>
     </PageHeaderWrapper>
   );
